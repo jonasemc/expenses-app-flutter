4656a0efc885085f683cf84b4dbed4700064b2cc
Jonas <jonasmirandaa21@gmail.com>
2024-05-23 18:20:09 -0300

    feat: create widget for transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,17 +15,73 @@ class ExpensesApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
+
+  final _transactions = [
+    Transaction(
+        id: '1', title: 'Teclado k500', value: 110, date: DateTime.now()),
+    Transaction(
+        id: '2', title: 'curso de .net', value: 24.99, date: DateTime.now()),
+    Transaction(id: '3', title: 'Monitor 24P', value: 699, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Initial Version'),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Despesas Pessoais'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Card(
+              color: Colors.purple,
+              elevation: 5,
+              child: Text('Grafico'),
+            ),
+            Column(
+                children: _transactions.map((tr) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 15, vertical: 10),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      )),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        tr.value.toString(),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(tr.title),
+                        Text(tr.date.toString())
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList())
+          ],
+        )
+        //,
+        );
   }
 }
